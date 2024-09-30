9116cf45e88e8696eb07435fa95be1abdf4aa302
Saeed Heydari <saeedhpro@gmail.com>
2024-10-01 00:18:40 +0330

    fix

--- dep.sh
+++ dep.sh
@@ -1,1 +1,1 @@
-curl -X POST "https://api.telegram.org/bot7415076812:AAEqyQE_M13mYmDn8wbxE3U-d6-uoYHe_2o/setWebhook?url=https://57e1-2-182-8-249.ngrok-free.app/webhook"
+curl -X POST "https://api.telegram.org/bot7415076812:AAEqyQE_M13mYmDn8wbxE3U-d6-uoYHe_2o/setWebhook?url=https://f6ee-5-115-95-83.ngrok-free.app/webhook"
